5abc2141f37cf1bf35eb2b58a82a2b87b603104d
YOOHOO <yoohoo_030@naver.com>
2024-08-29 19:10:46 +0900

    feat: post페이지에서 댓글을 보여주는 기능 추가

--- front/src/pages/Post.js
+++ front/src/pages/Post.js
@@ -8,6 +8,7 @@ const Post = () => {
 	const [title, setTitle] = useState('');
 	const [content, setContent] = useState('');
 	const [createdAt, setCreatedAt] = useState('');
+	const [comments, setComments] = useState([]);
 
 	useEffect(() => {
 		axios
@@ -21,7 +22,15 @@ const Post = () => {
 				setCreatedAt(res.data.createdAt);
 			})
 			.catch((error) => {
-				console.error('There was an error fetching the data!', error);
+				console.error('글 내용을 불러오는 중 에러가 발생했습니다.', error);
+			});
+		axios
+			.get(`http://localhost:4000/post/${id}/comment`)
+			.then((res) => {
+				setComments(res.data);
+			})
+			.catch((error) => {
+				console.error('댓글 목록을 불러오는 중 에러가 발생했습니다.', error);
 			});
 	}, []);
 	return (
@@ -29,8 +38,21 @@ const Post = () => {
 			<h2>Daily talk</h2>
 			<div>{title}</div>
 			<div>{nick}</div>
-			<div>{createdAt}</div>
+			<div>{new Date(createdAt).toLocaleString()}</div>
 			<div>{content}</div>
+			<ul>
+				{comments.map((comment) => (
+					<li key={comment.id}>
+						<p>{comment.User.nick}</p>
+						<p>{new Date(comment.createdAt).toLocaleString()}</p>
+						<p>{comment.content}</p>
+						<br />
+					</li>
+				))}
+			</ul>
+			<button type="button" onClick={() => window.history.back()}>
+				목록으로
+			</button>
 		</div>
 	);
 };
